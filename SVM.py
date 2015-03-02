52fd6b87e668a6dc07d32bf1724086e7cb6f7d11
John Robertson <jjrob13@gmail.com>
2015-03-02 00:02:44 -0700

    Create training and test samples.  The first run is about 55% accurate on the test data.

--- SVM.py
+++ SVM.py
@@ -5,20 +5,42 @@ f = open('crx.data.csv')
 
 csv_f = csv.reader(f);
 
-entry_array = [];
-
+test_entry_array = [];
+validation_entry_array = [];
+i = 0;
 for row in csv_f:
-	entry_array.append(Entry.Entry(row));
+	if i % 2 == 0:
+		test_entry_array.append(Entry.Entry(row));
+	else:
+		validation_entry_array.append(Entry.Entry(row));
+	i = i + 1;
 
-collection = EntryCollection.EntryCollection(entry_array);
-
+test_collection = EntryCollection.EntryCollection(test_entry_array);
+validation_collection = EntryCollection.EntryCollection(validation_entry_array);
 
 
 clf = svm.SVC();
 
-X_Y = collection.get_X_Y_vector_tuple();
+X_Y_test = test_collection.get_X_Y_vector_tuple();
 
-X = X_Y[0];
-Y = numpy.transpose(X_Y[1]);
+X = X_Y_test[0];
+Y = numpy.transpose(X_Y_test[1]);
 
 clf.fit(X, Y);
+
+
+X_Y_validation = validation_collection.get_X_Y_vector_tuple();
+
+
+X_val = X_Y_validation[0];
+Y_val = numpy.transpose(X_Y_validation[1]);
+correct = 0;
+
+i = 0;
+while i != len(X_val):
+	if clf.predict(X_val[i])[0] == Y_val[i]:
+		correct = correct + 1;
+
+	i = i + 1;
+
+print (correct * 1.0)/i * 100;
