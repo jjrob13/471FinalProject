a322d656fbb74d48d89f9a906b5ca69e95f8898b
John Robertson <jjrob13@gmail.com>
2015-04-03 22:39:31 -0700

    Add refractor.py for plotting classifier comparison.

--- Part2/src/DataSet.py
+++ Part2/src/DataSet.py
@@ -1,6 +1,11 @@
 import Entry, csv, random
+
+class XY_Set:
+	def __init__(self, x, y):
+		self.x = x;
+		self.y = y;	
 class DataSet:
-	def __init__(self, filename = 'crx.data.csv'):
+	def __init__(self, training_size_percentage = 0.5,  filename = 'crx.data.csv'):
 		f = open(str(filename))
 
 		csv_f = csv.reader(f);
@@ -8,17 +13,15 @@ class DataSet:
 		self.all_entries = [];
 		for row in csv_f:
 			self.all_entries.append(Entry.Entry(row));
-
-
-
-	def get_training_and_test_set_tuple(self, training_set_size):
-		if(training_set_size > len(self.all_entries)):
-			raise ValueExcept('Size of training set must be less than or equal to population size');
-
+		
+		self.update_training_size(training_size_percentage);
+	def update_training_size(self, training_size_percentage):
+	
 		random.shuffle(self.all_entries);
-		training_set = self.all_entries[0:training_set_size];
-
-		testing_set = self.all_entries[training_set_size:];
-
-		return (training_set, testing_set);
-
+		training_size = int(training_size_percentage * len(self.all_entries)) ;
+		
+		training_data = self.all_entries[:training_size];
+		test_data = self.all_entries[training_size:];
+		
+		self.testing_set = XY_Set(Entry.get_X_Y_tuple_from_entries(test_data)[0], Entry.get_X_Y_tuple_from_entries(test_data)[1]);			
+		self.training_set = XY_Set(Entry.get_X_Y_tuple_from_entries(training_data)[0], Entry.get_X_Y_tuple_from_entries(training_data)[1]);	
---
+++ Part2/src/refractor.py
@@ -0,0 +1,35 @@
+import DataSet
+from sklearn.svm import SVC 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from matplotlib import pyplot as plt
+
+data = DataSet.DataSet();
+axes = [];
+for i in range(1, 10):
+	data.update_training_size(i * .1);
+	svm = SVC(kernel = 'linear');
+	svm.fit(data.training_set.x, data.training_set.y);
+	svm_score = svm.score(data.testing_set.x, data.testing_set.y);
+
+	nb = GaussianNB();
+	nb.fit(data.training_set.x, data.training_set.y);
+	nb_score = nb.score(data.testing_set.x, data.testing_set.y);
+
+
+	dt = DecisionTreeClassifier();
+	dt.fit(data.training_set.x, data.training_set.y);
+	dt_score = dt.score(data.testing_set.x, data.testing_set.y);
+
+	fig = plt.figure(i);
+	ax = fig.add_subplot(111);
+	x = range(3);
+	rects = [];
+	rects.append(ax.bar(0, svm_score, color='r', label="SVM"));
+	rects.append(ax.bar(1, nb_score, color='b', label="NB"));
+	rects.append(ax.bar(2, dt_score, color='g', label="DT"));
+	labels = ("", "SVM", "",  "NB", "", "DT");
+	ax.set_xticklabels(labels);
+	ax.set_title("Training With {0}% of Data".format(str(i * 10)))
+	axes.add(ax);
+plt.show();
